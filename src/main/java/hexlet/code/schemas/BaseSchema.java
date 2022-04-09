e8a1476e804ac4721e422236b513c8c1f44170f0
IVF13_notebook <iw.gordienko@yandex.ru>
2022-04-09 16:15:20 +0300

    Refactor isValid method to remove code smells

--- src/main/java/hexlet/code/schemas/BaseSchema.java
+++ src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -8,11 +8,12 @@ public abstract class BaseSchema {
     private final Map<Checks, Predicate<Object>> checkList = new HashMap<>();
 
     public final boolean isValid(Object objectToValidate) {
-        if (!checkList.containsKey(Checks.REQUIRED) && objectToValidate == null) {
-            return true;
-        }
 
-        if (checkList.containsKey(Checks.REQUIRED) && !checkList.get(Checks.REQUIRED).test(objectToValidate)) {
+        if (!checkList.containsKey(Checks.REQUIRED)) {
+            if (objectToValidate == null) {
+                return true;
+            }
+        } else if (!checkList.get(Checks.REQUIRED).test(objectToValidate)) {
             return false;
         }
 
@@ -24,8 +25,7 @@ public abstract class BaseSchema {
                 }
             }
 
-        } catch (ClassCastException | IllegalArgumentException e) {
-            return true;
+        } catch (ClassCastException | IllegalArgumentException ignored) {
         }
 
         return true;
